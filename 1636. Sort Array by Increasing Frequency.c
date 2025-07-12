2d346f9887b3a5c50964cb4d202e691b4ac067ea
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-07-12 22:22:51 +0530

    initial commit

---
+++ 1636. Sort Array by Increasing Frequency.c
@@ -0,0 +1,54 @@
+/*  Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+Return the sorted array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,2,2,3]
+Output: [3,1,1,2,2,2]
+Explanation: '3' has a frequency of 1, '1' has a frequency of 2, and '2' has a frequency of 3.
+Example 2:
+
+Input: nums = [2,3,1,3,2]
+Output: [1,3,3,2,2]
+Explanation: '2' and '3' both have a frequency of 2, so they are sorted in decreasing order.
+Example 3:
+
+Input: nums = [-1,1,-6,4,5,-6,1,4,1]
+Output: [5,-1,4,4,-6,-6,1,1,1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+-100 <= nums[i] <= 100  */
+
+
+
+
+class Solution {
+public:
+    static unordered_map<int, int> um;
+    static bool ascFrq(int s1, int s2) {
+        if (um[s1] < um[s2]) {
+            return true;
+        } else if (um[s1] == um[s2]) {
+            return s1 > s2;  
+        } else {
+            return false;
+        }
+    }
+
+    vector<int> frequencySort(vector<int>& nums) {
+        um.clear();  
+        for (int i = 0; i < nums.size(); i++) {
+            um[nums[i]]++;
+        }
+        sort(nums.begin(), nums.end(), ascFrq);
+        return nums;
+    }
+};
+
+unordered_map<int, int> Solution::um;
