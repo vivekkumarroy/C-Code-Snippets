7375a7c5e5cff28068bc4f967c5f1a8ed5b1a7de
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-08-14 20:33:22 +0530

    initial commit

---
+++ Wave Array.c
@@ -0,0 +1,36 @@
+/*  Given an sorted array arr[] of integers. Sort the array into a wave-like array(In Place). In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5] ..... and so on. If there are multiple solutions, find the lexicographically smallest one.
+
+Note: The given array is sorted in ascending order, and modify the given array in-place without returning a new array.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [2, 1, 4, 3, 5]
+Explanation: Array elements after sorting it in the waveform are 2, 1, 4, 3, 5.
+Input: arr[] = [2, 4, 7, 8, 9, 10]
+Output: [4, 2, 8, 7, 10, 9]
+Explanation: Array elements after sorting it in the waveform are 4, 2, 8, 7, 10, 9.
+Input: arr[] = [1]
+Output: [1]
+Constraints:
+1 ≤ arr.size ≤ 106
+0 ≤ arr[i] ≤109  */
+
+
+
+
+
+
+
+class Solution {
+  public:
+    void sortInWave(vector<int>& arr) {
+        // code here
+        
+        for(int i = 0;i < arr.size()/2;i++){
+            int temp = arr[2*i];
+            arr[2*i] = arr[2*i+1];
+            arr[2*i+1] = temp;
+        }
+    }
+};
