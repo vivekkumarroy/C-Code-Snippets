38301c0203e16f062952078e392c358818ef5cb0
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-08-28 21:29:37 +0530

    initial commit

---
+++ Parenthesis Checker.c
@@ -0,0 +1,54 @@
+/*   Given a string s, composed of different combinations of '(' , ')', '{', '}', '[', ']', verify the validity of the arrangement.
+An input string is valid if:
+
+         1. Open brackets must be closed by the same type of brackets.
+         2. Open brackets must be closed in the correct order.
+
+Examples :
+
+Input: s = "[{()}]"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "[()()]{}"
+Output: true
+Explanation: All the brackets are well-formed.
+Input: s = "([]"
+Output: false
+Explanation: The expression is not balanced as there is a missing ')' at the end.
+Input: s = "([{]})"
+Output: false
+Explanation: The expression is not balanced as there is a closing ']' before the closing '}'.
+Constraints:
+1 ≤ s.size() ≤ 106
+s[i] ∈ {'{', '}', '(', ')', '[', ']'}    */
+
+
+
+
+
+
+
+class Solution {
+  public:
+    bool isBalanced(string& k) {
+        // code here
+    stack<char> st; 
+    for (char c : k) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        }
+        else if (c == ')' || c == '}' || c == ']') {
+            if (st.empty()) return false; 
+            char top = st.top();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '[')) {
+                return false;
+            }
+            st.pop(); 
+        }
+    }
+    return st.empty(); 
+        
+    }
+};
