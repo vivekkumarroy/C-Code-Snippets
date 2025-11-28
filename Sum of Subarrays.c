f9c21147c0300c223fcd309a4f5834e60c21c338
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-11-28 22:09:31 +0530

    initial commit

---
+++ Sum of Subarrays.c
@@ -0,0 +1,37 @@
+/*  Given an array arr[], find the sum of all the subarrays of the given array.
+
+Note: It is guaranteed that the total sum will fit within a 32-bit integer range.
+
+Examples:
+
+Input: arr[] = [1, 2, 3] 
+Output: 20
+Explanation: All subarray sums are: [1] = 1, [2] = 2, [3] = 3, [1, 2] = 3, [2, 3] = 5, [1, 2, 3] = 6. Thus total sum is 1 + 2 + 3 + 3 + 5 + 6 = 20.
+Input: arr[] = [1, 3]
+Output: 8
+Explanation: All subarray sums are: [1] = 1, [3] = 3, [1, 3] = 4. Thus total sum is 1 + 3 + 4 = 8.
+Constraints :
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 104
+
+  */
+
+
+
+
+
+
+
+  class Solution {
+  public:
+    int subarraySum(vector<int>& arr) {
+        // code here
+        int sum = 0;
+        int n = arr.size();
+        for(int i = 0;i<n;i++){
+            sum += (n-i)*(i+1)*arr[i];
+            
+        } 
+        return sum;
+    }
+};
