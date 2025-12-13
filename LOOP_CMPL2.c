d21246ce80b7bcc0646d11f0f273465ac0befde0
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-12-13 22:01:02 +0530

    initial commit

---
+++ LOOP_CMPL2.c
@@ -0,0 +1,23 @@
+What is the time complexity of the following code :
+
+int i, j, k = 0;
+for (i = n/2; i <= n; i++) {
+    for (j = 2; j <= n; j = j * 2) {
+         k = k + n/2;
+    }
+}
+Show similar questions
+REC_CMPL2
+6 minutes easy
+Asked in:
+Extracting Numbers
+27 minutes very easy
+Asked in:
+Integers in Strings
+23 minutes medium
+Asked in:
+Θ(n)
+Θ(nLogn)
+Θ(n^2)
+Θ(n^2 / Logn)
+Θ(n^2Logn)
