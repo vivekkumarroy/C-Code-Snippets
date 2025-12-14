1074b46db76221f518c9935bb1e1a7e134cd6f13
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-12-14 21:49:41 +0530

    initial commit

---
+++ CHOOSE3.c
@@ -0,0 +1,27 @@
+In a competition, four different functions are observed. All the functions use a single for loop and within the for loop, same set of statements are executed.
+
+Consider the following for loops:
+
+  A) for(i = 0; i < n; i++)
+ 
+  B) for(i = 0; i < n; i += 2)
+ 
+  C) for(i = 1; i < n; i *= 2)
+ 
+  D) for(i = n; i > -1; i /= 2)
+If n is the size of input(positive), which function is the most efficient? In other words, which loop completes the fastest.
+
+Show similar questions
+REC_CMPL2
+6 minutes easy
+Asked in:
+Extracting Numbers
+27 minutes very easy
+Asked in:
+Integers in Strings
+23 minutes medium
+Asked in:
+A
+B
+C
+D
