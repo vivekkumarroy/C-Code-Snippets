d52406b96de3fb3a98bd7ba7ae73b6cf12866d53
vivek kumar roy <vivekkumarroy5355@gmail.com>
2025-12-15 22:24:25 +0530

    initial commit

---
+++ CHOOSE2.c
@@ -0,0 +1,24 @@
+Which of the given options provides the increasing order of complexity of functions f1, f2, f3 and f4:
+
+f1(n) = 2^n
+
+f2(n) = n^(3/2)
+
+f3(n) = nLogn
+
+f4(n) = n^(Logn)
+
+Show similar questions
+REC_CMPL2
+6 minutes easy
+Asked in:
+Extracting Numbers
+27 minutes very easy
+Asked in:
+Integers in Strings
+23 minutes medium
+Asked in:
+f3, f2, f4, f1
+f3, f2, f1, f4
+f2, f3, f1, f4
+f2, f3, f4, f1
