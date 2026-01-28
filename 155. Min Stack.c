15ba930860554395abae402e79d53952a9e3f86f
vivek kumar roy <vivekkumarroy5355@gmail.com>
2026-01-28 23:21:23 +0530

    initial commit

---
+++ 155. Min Stack.c
@@ -0,0 +1,72 @@
+/* Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+ 
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+ 
+
+Constraints:
+
+-231 <= val <= 231 - 1
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+At most 3 * 104 calls will be made to push, pop, top, and getMin.    */
+
+
+
+
+
+
+class MinStack {
+public:
+    MinStack() {
+        stk2.push(INT_MAX);
+    }
+    
+    void push(int val) {
+        stk1.push(val);
+        stk2.push(min(val, stk2.top()));
+    }
+    
+    void pop() {
+        stk1.pop();
+        stk2.pop();
+    }
+    
+    int top() {
+        return stk1.top();
+    }
+    
+    int getMin() {
+        return stk2.top();
+    }
+
+private:
+    stack<int> stk1;
+    stack<int> stk2;
+};
