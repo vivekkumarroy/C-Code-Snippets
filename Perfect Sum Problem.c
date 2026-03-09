4750d2916bf5f7efc8452f84e63fba4de4d189e9
vivek kumar roy <vivekkumarroy5355@gmail.com>
2026-03-09 22:03:44 +0530

    initial commit

---
+++ Perfect Sum Problem.c
@@ -0,0 +1,38 @@
+/*   Given an array arr of non-negative integers and an integer target, the task is to count all subsets of the array whose sum is equal to the given target.
+
+Examples:
+
+Input: arr[] = [5, 2, 3, 10, 6, 8], target = 10
+Output: 3
+Explanation: The subsets {5, 2, 3}, {2, 8}, and {10} sum up to the target 10.
+Input: arr[] = [2, 5, 1, 4, 3], target = 10
+Output: 3
+Explanation: The subsets {2, 1, 4, 3}, {5, 1, 4}, and {2, 5, 3} sum up to the target 10.
+Input: arr[] = [5, 7, 8], target = 3
+Output: 0
+Explanation: There are no subsets of the array that sum up to the target 3.
+Input: arr[] = [35, 2, 8, 22], target = 0
+Output: 1
+Explanation: The empty subset is the only subset with a sum of 0.
+Constraints:
+1 ≤ arr.size() ≤ 103
+0 ≤ arr[i] ≤ 103
+0 ≤ target ≤ 103  */
+
+
+
+
+class Solution {
+public:
+    int perfectSum(vector<int>& arr, int target) {
+        // const int MOD = 1000000007;
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int num : arr) {
+            for (int sum = target; sum >= num; --sum) {
+                dp[sum] = (dp[sum] + dp[sum - num]);
+            }
+        }
+        return dp[target];
+    }
+};
