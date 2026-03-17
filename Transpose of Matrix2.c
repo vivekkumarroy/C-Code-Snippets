38008692b8a9407d521aa847672f10681a4c7b23
vivek kumar roy <vivekkumarroy5355@gmail.com>
2026-03-18 01:52:50 +0530

    initial commit

--- Transpose of Matrix2.c
+++ Transpose of Matrix2.c
@@ -1,31 +1,3 @@
-/*  You are given a square matrix of size n x n. Your task is to find the transpose of the given matrix.
-The transpose of a matrix is obtained by converting all the rows to columns and all the columns to rows.
-
-Examples :
-
-Input: mat[][] = [[1, 1, 1, 1],
-                [2, 2, 2, 2],
-                [3, 3, 3, 3],
-                [4, 4, 4, 4]]
-Output: [[1, 2, 3, 4],
-       [1, 2, 3, 4],
-       [1, 2, 3, 4],
-       [1, 2, 3, 4]]
-Explanation: Converting rows into columns and columns into rows.
-Input: mat[][] =  [[1, 2],
-                 [9, -2]]
-Output: [[1, 9],
-        [2, -2]]
-Explanation: Converting rows into columns and columns into rows.
-Constraints:
-1 ≤ n ≤ 103
--109 ≤ mat[i][j] ≤109   */
-
-
-
-
-
-
 
 class Solution {
   public:
