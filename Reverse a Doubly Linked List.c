d6f87ecb1506d6586ffbe066803b852518ec55b8
vivek kumar roy <vivekkumarroy5355@gmail.com>
2026-03-21 22:55:44 +0530

    initial commit

---
+++ Reverse a Doubly Linked List.c
@@ -0,0 +1,51 @@
+/*   You are given the head of a doubly linked list. You have to reverse the doubly linked list and return its head.
+
+Examples:
+
+Input:
+   
+Output: 5 <-> 4 <-> 3
+Explanation: After reversing the given doubly linked list the new list will be 5 <-> 4 <-> 3.
+   
+Input: 
+   
+Output: 196 <-> 59 <-> 122 <-> 75
+Explanation: After reversing the given doubly linked list the new list will be 196 <-> 59 <-> 122 <-> 75.
+   
+Constraints:
+1 ≤ number of nodes ≤ 106
+0 ≤ node->data ≤ 104   */
+
+
+
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val) {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    Node *reverse(Node *head) {
+        // code here
+        
+       Node *rh = NULL;
+        while(head){
+            Node *temp = head;
+            head = head -> next;
+            temp -> prev = head;
+            temp -> next = rh;
+            rh = temp;
+        }
+        rh -> prev = NULL;
+        return rh;
+    }
+};
